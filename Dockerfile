27468d2c4d6f050de1490c1af80ce5230d9f3f48
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2022-01-21 18:15:39 -0300

    Aprendendo a usar as traits de rede

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM rust:latest as builder
-WORKDIR /usr/src/myapp
+WORKDIR /usr/src/aprendendorust
 COPY . .
 RUN cargo install --path .
 
@@ -7,3 +7,4 @@ FROM debian:buster-slim
 #RUN apt-get update && apt-get install -y extra-runtime-dependencies && rm -rf /var/lib/apt/lists/*
 COPY --from=builder /usr/local/cargo/bin/aprendendorust /usr/local/bin/aprendendorust
 CMD ["aprendendorust"]
+EXPOSE 8080
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Write;
+use std::net::{TcpListener, TcpStream, Shutdown};
+
+fn handle_client(mut stream: TcpStream) {
+    println!("O servidor recebeu uma conexão");
+
+    let data = b"<h1>oi</h1>";
+
+    let bytes_escritos = stream.write_all(data);
+    stream.shutdown(Shutdown::Both).expect("shutdown call failed");
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
 }
